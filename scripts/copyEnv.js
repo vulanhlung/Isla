9fe8850fb0862ae8fcfb5fe2ec89d4a70b6a7d9c
vulanhlung <vulanhlung@gmail.com>
2026-05-30 10:06:46 +0700

    fix deploy

--- scripts/copyEnv.js
+++ scripts/copyEnv.js
@@ -3,20 +3,20 @@ import path from 'path'
 
 const root = path.resolve('./')
 const envFile = path.join(root, '.env')
-const envProductionFile = path.join(root, '.env.production')
+const envExampleFile = path.join(root, '.env.example')
 
 function copyEnv() {
   if (fs.existsSync(envFile)) {
     return
   }
 
-  if (!fs.existsSync(envProductionFile)) {
-    console.warn('.env.production not found, skipping env copy.')
+  if (!fs.existsSync(envExampleFile)) {
+    console.warn('.env.example not found, skipping env copy.')
     return
   }
 
-  fs.copyFileSync(envProductionFile, envFile)
-  console.log('Copied .env.production to .env')
+  fs.copyFileSync(envExampleFile, envFile)
+  console.log('Copied .env.example to .env')
 }
 
 copyEnv()
